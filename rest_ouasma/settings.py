51c595c5ad67ef6e4524fbfcc3105d4c475cb8e6
wafi <mameri.wafi@gmail.com>
2020-09-18 14:37:45 +0100

    add heroku port

--- rest_ouasma/settings.py
+++ rest_ouasma/settings.py
@@ -173,4 +173,5 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 }
 
 # ACCOUNT_ADAPTER = 'accounts.adapter.UserAccountAdapter'
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
