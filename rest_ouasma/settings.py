02ec8b0370f01d7e9c854e13e0459219f8c6002e
wafi <mameri.wafi@gmail.com>
2020-09-18 12:39:16 +0100

    add localhost

--- rest_ouasma/settings.py
+++ rest_ouasma/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'polvnr)t=bi)4$guts@m)e3+6%@v(03eh)tkhd(u$_b_!2jvt1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['appapiwafi.herokuapp.com']
 
