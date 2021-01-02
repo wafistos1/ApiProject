1a1302a4204a792b50141271bee1ec19a008e50b
wafi <mameri.wafi@gmail.com>
2021-01-02 11:32:12 +0100

    add env production

--- rest_ouasma/wsgi.py
+++ rest_ouasma/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_ouasma.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_ouasma.settings.pro')
 
 application = get_wsgi_application()
