374e86ea2d4323482de12dd435614b74690ada6d
wafi <mameri.wafi@gmail.com>
2021-01-02 11:07:46 +0100

    modified settings

--- rest_ouasma/settings.py
+++ rest_ouasma/settings.py
@@ -129,8 +129,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ar-ea'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'ar-ea'
 
 TIME_ZONE = 'UTC'
 
